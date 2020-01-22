26ba319aa8cc94af4e3bbed5159eba6c60b612fe
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2020-01-22 14:26:55 +0530

    Creating components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/base/base.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { TasksComponent } from './components/base/tasks/tasks.component';
+import { ExpenseGroupsComponent } from './components/base/expense-groups/expense-groups.component';
+import { BillsComponent } from './components/base/bills/bills.component';
+import { MappingsComponent } from './components/base/mappings/mappings.component';
+import { SettingsComponent } from './components/base/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    LoginComponent,
+    LogoutComponent,
+    TasksComponent,
+    ExpenseGroupsComponent,
+    BillsComponent,
+    MappingsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/base/expense-groups/expense-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-groups',
+  templateUrl: './expense-groups.component.html',
+  styleUrls: ['./expense-groups.component.css']
+})
+export class ExpenseGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

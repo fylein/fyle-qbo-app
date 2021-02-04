363f0c476018bb98f3f64e3193ea56674b3e7035
Nilesh Pant <58652823+NileshPant1999@users.noreply.github.com>
2021-02-04 15:11:11 +0530

    Moving Filtering Logic to backend (#39)
    * sending expense ids in api call
    
    * frontend changes
    
    * variable name fix
    
    * lint fix
    
    * filtering logic change
    
    * lint fix
    
    * lint fix
    
    * minor fix
    
    * sending task_type for client
    
    * using django next insted of count
    
    * fix

--- src/app/core/services/tasks.service.ts
+++ src/app/core/services/tasks.service.ts
@@ -11,22 +11,34 @@ import { WorkspaceService } from './workspace.service';
 export class TasksService {
   constructor(
     private apiService: ApiService,
-    private workspaceService: WorkspaceService) {}
+    private workspaceService: WorkspaceService) { }
 
-  getTasks(limit: number, offset: number, status: string): Observable<TaskResponse> {
+  getTasks(limit: number, status: string, expenseGroupIds: number[], taskType: string[], next: string): Observable<TaskResponse> {
     const workspaceId = this.workspaceService.getWorkspaceId();
-    return this.apiService.get(
-      `/workspaces/${workspaceId}/tasks/all/`, {
-        limit,
-        offset,
-        status
-      }
-    );
+    const offset = 0;
+    const apiParams = {
+      limit,
+      offset,
+      status
+    };
+    if (expenseGroupIds && taskType) {
+      const expenseKey = 'expense_group_ids';
+      const typeKey = 'task_type';
+      apiParams[expenseKey] = expenseGroupIds;
+      apiParams[typeKey] = taskType;
+    }
+
+    if (next) {
+      return this.apiService.get(next.split('api')[1], {});
+    } else {
+      return this.apiService.get(
+        `/workspaces/${workspaceId}/tasks/all/`, apiParams
+      );
+    }
   }
 
-  getAllTasks(status: string): Observable<TaskResponse> {
+  getAllTasks(status: string, expenseGroupIds: number[] = null, taskType: string[] = null): Observable<TaskResponse> {
     const limit = 500;
-    const offset = 0;
     const allTasks: TaskResponse = {
       count: 0,
       next: null,
@@ -34,20 +46,20 @@ export class TasksService {
       results: []
     };
 
-    return from(this.getAllTasksInternal(limit, offset, status, allTasks));
+    return from(this.getAllTasksInternal(limit, status, expenseGroupIds, taskType, allTasks));
   }
   // TODO: remove promises and do with rxjs observables
-  private getAllTasksInternal(limit: number, offset: number, status: string, allTasks: TaskResponse): Promise<TaskResponse> {
+  private getAllTasksInternal(limit: number, status: string, expenseGroupIds: number[], taskType: string[], allTasks: TaskResponse): Promise<TaskResponse> {
     const that = this;
-    return that.getTasks(limit, offset, status).toPromise().then((taskResponse) => {
-      if (allTasks.count === 0 ) {
+    return that.getTasks(limit, status, expenseGroupIds, taskType, allTasks.next).toPromise().then((taskResponse) => {
+      if (allTasks.count === 0) {
         allTasks = taskResponse;
       } else {
         allTasks.results = allTasks.results.concat(taskResponse.results);
       }
 
-      if (allTasks.results.length < allTasks.count) {
-        return that.getAllTasksInternal(limit, offset + 500, status, allTasks);
+      if (taskResponse.next) {
+        return that.getAllTasksInternal(limit, status, expenseGroupIds, taskType, allTasks);
       } else {
         return allTasks;
       }

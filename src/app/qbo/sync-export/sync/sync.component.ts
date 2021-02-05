2e47bd33abf3d0d1c2cf195bfe8b4a2582ef533f
KirtiGautam <gautamkirti8c@gmail.com>
2021-02-05 12:41:45 +0530

    TaskType declaration in method calling

--- src/app/qbo/sync-export/sync/sync.component.ts
+++ src/app/qbo/sync-export/sync/sync.component.ts
@@ -41,8 +41,9 @@ export class SyncComponent implements OnInit {
 
   checkSyncStatus() {
     const that = this;
+    const taskType = ['FETCHING_EXPENSES'];
     interval(3000).pipe(
-      switchMap(() => from(that.taskService.getAllTasks('ALL'))),
+      switchMap(() => from(that.taskService.getAllTasks('ALL', [], taskType))),
       takeWhile((response) => response.results.filter(task => task.status === 'IN_PROGRESS'  && task.type === 'FETCHING_EXPENSES').length > 0, true)
     ).subscribe((res) => {
       if (res.results.filter(task => task.status === 'COMPLETE'  && task.type === 'FETCHING_EXPENSES').length === 1) {

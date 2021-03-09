f53c19ded9b88da33bb60faa9f848981bd21e7af
ashwinthanaraj <ashwin.t@fyle.in>
2021-03-09 16:43:44 +0530

    Turn off auto create if auto map is configured for employee code

--- src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
+++ src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
@@ -235,7 +235,7 @@ export class GeneralConfigurationComponent implements OnInit {
 
     showAutoCreateOption(autoMapEmployees) {
       const that = this;
-      if (autoMapEmployees) {
+      if (autoMapEmployees && autoMapEmployees !== 'EMPLOYEE_CODE') {
         that.showAutoCreate = true;
       } else {
         that.showAutoCreate = false;

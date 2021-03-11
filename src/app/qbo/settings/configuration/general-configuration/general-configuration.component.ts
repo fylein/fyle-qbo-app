603eedccd2b0443b9446b4d696244f9c2483a1df
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-03-11 18:09:44 +0530

    Fix auto create fix (#59)

--- src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
+++ src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
@@ -162,6 +162,7 @@ export class GeneralConfigurationComponent implements OnInit {
 
       that.generalSettingsForm.controls.employees.valueChanges.subscribe((employeeMappedTo) => {
         that.expenseOptions = that.getExpenseOptions(employeeMappedTo);
+        that.showAutoCreateOption(null);
         that.generalSettingsForm.controls.reimburExpense.reset();
       });
     });
@@ -243,16 +244,16 @@ export class GeneralConfigurationComponent implements OnInit {
 
     showAutoCreateOption(autoMapEmployees) {
       const that = this;
-      if (autoMapEmployees && autoMapEmployees !== 'EMPLOYEE_CODE') {
-        that.showAutoCreate = true;
-      } else {
-        that.disableAutoCreate();
-      }
-
       if (that.employeeFieldMapping && that.employeeFieldMapping.destination_field === 'EMPLOYEE') {
         that.disableAutoCreate();
       }
       if (that.generalSettingsForm.value.employees && that.generalSettingsForm.value.employees === 'EMPLOYEE') {
+        that.disableAutoCreate();
+      }
+
+      if (autoMapEmployees && autoMapEmployees !== 'EMPLOYEE_CODE') {
+        that.showAutoCreate = true;
+      } else {
         that.disableAutoCreate();
       }
     }

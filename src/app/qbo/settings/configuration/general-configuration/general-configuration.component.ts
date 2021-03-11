00f97c370963b6a38d87e8b1d19a2aa08708f8fe
ashwinthanaraj <ashwin.t@fyle.in>
2021-03-11 17:35:35 +0530

    Fix conditional auto create

--- src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
+++ src/app/qbo/settings/configuration/general-configuration/general-configuration.component.ts
@@ -162,7 +162,6 @@ export class GeneralConfigurationComponent implements OnInit {
 
       that.generalSettingsForm.controls.employees.valueChanges.subscribe((employeeMappedTo) => {
         that.expenseOptions = that.getExpenseOptions(employeeMappedTo);
-        that.showAutoCreateOption(null);
         that.generalSettingsForm.controls.reimburExpense.reset();
       });
     });
@@ -237,13 +236,24 @@ export class GeneralConfigurationComponent implements OnInit {
       }
     }
 
+    disableAutoCreate() {
+      this.showAutoCreate = false;
+      this.generalSettingsForm.controls.autoCreateDestinationEntity.setValue(false);
+    }
+
     showAutoCreateOption(autoMapEmployees) {
       const that = this;
       if (autoMapEmployees && autoMapEmployees !== 'EMPLOYEE_CODE') {
         that.showAutoCreate = true;
       } else {
-        that.showAutoCreate = false;
-        that.generalSettingsForm.controls.autoCreateDestinationEntity.setValue(false);
+        that.disableAutoCreate();
+      }
+
+      if (that.employeeFieldMapping && that.employeeFieldMapping.destination_field === 'EMPLOYEE') {
+        that.disableAutoCreate();
+      }
+      if (that.generalSettingsForm.value.employees && that.generalSettingsForm.value.employees === 'EMPLOYEE') {
+        that.disableAutoCreate();
       }
     }
 

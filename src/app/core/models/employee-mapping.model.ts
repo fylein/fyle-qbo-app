a829d05657db84c24f77a415c6f215ba5a2079cc
ashwinthanaraj <ashwin.t@fyle.in>
2021-07-27 21:09:36 +0530

    Update type

--- src/app/core/models/employee-mapping.model.ts
+++ src/app/core/models/employee-mapping.model.ts
@@ -4,22 +4,10 @@ import { MappingSource } from './mapping-source.model';
 /* tslint:disable */
 export type EmployeeMapping = {
   id?: number;
-  source_employee: {
-    id: number;
-    value?: string;
-  } | MappingSource;
-  destination_employee?: {
-    id: number;
-    value?: string;
-  } | MappingDestination;
-  destination_vendor?: {
-    id: number;
-    value?: string;
-  } | MappingDestination;
-  destination_card_account?: {
-    id: number;
-    value?: string;
-  } | MappingDestination;
+  source_employee: MappingSource;
+  destination_employee?: MappingDestination;
+  destination_vendor?: MappingDestination;
+  destination_card_account?: MappingDestination;
   created_at?: Date;
   updated_at?: Date;
   workspace: number;

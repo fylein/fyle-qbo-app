f7d87a696de9e8dcde2a861b2a29411ba68abe09
labhvam5 <labhvamkrsharma@gmail.com>
2021-09-13 18:40:45 +0530

    fix for choosing wrong QBO account

--- src/app/qbo/settings/qbo-callback/qbo-callback.component.ts
+++ src/app/qbo/settings/qbo-callback/qbo-callback.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SettingsService } from 'src/app/core/services/settings.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-qbo-callback',
@@ -8,11 +9,11 @@ import { SettingsService } from 'src/app/core/services/settings.service';
   styleUrls: ['./qbo-callback.component.scss']
 })
 export class QBOCallbackComponent implements OnInit {
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private settingsService: SettingsService) { }
+    private settingsService: SettingsService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     const that = this;
@@ -20,6 +21,11 @@ export class QBOCallbackComponent implements OnInit {
     const code: string = that.route.snapshot.queryParams.code;
     const realmId: string = that.route.snapshot.queryParams.realmId;
     that.settingsService.connectQBO(workspaceId, code, realmId).subscribe(() => {
+      that.router.navigateByUrl(`workspaces/${workspaceId}/dashboard`);
+    },(error)=>{
+      that.snackBar.open(`Please choose the correct Quickbooks online account`,null, {
+        duration: 5000
+      });
       that.router.navigateByUrl(`workspaces/${workspaceId}/dashboard`);
     });
   }

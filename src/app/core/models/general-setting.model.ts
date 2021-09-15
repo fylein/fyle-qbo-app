b272238b52a052fa4998851c71f0f57a42b02461
Nilesh Pant <nileshpant6969@gmail.com>
2021-09-15 20:16:21 +0530

    change variable name

--- src/app/core/models/general-setting.model.ts
+++ src/app/core/models/general-setting.model.ts
@@ -5,7 +5,7 @@ export type GeneralSetting = {
   corporate_credit_card_expenses_object: string;
   import_projects?: boolean;
   import_categories: boolean;
-  forced_export?: boolean;
+  change_accounting_period?: boolean;
   sync_fyle_to_qbo_payments: boolean;
   sync_qbo_to_fyle_payments: boolean;
   auto_map_employees: string;

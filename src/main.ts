fce55032dfd15eb34f1fdeac033df65e50417aa6
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2021-09-24 16:02:47 +0530

    sentry performance changes (#115)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as Sentry from '@sentry/angular';
+import { Integrations as TracingIntegrations } from '@sentry/tracing';
 
 const hostname = window.location.hostname;
 const env = hostname.substring(0, hostname.indexOf('.'));
@@ -13,6 +14,9 @@ if (environment.sentry_dsn) {
     dsn: environment.sentry_dsn,
     release: environment.release,
     environment : env,
+    integrations: [new TracingIntegrations.BrowserTracing({
+      routingInstrumentation: Sentry.routingInstrumentation,
+    })],
     ignoreErrors: [
       'Non-Error exception captured'
     ],

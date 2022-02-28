7d461386c3d10b38aca966d9290493520968c0bb
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-02-28 11:03:43 +0530

    Changes to support Docker Compose via Integrations Central (#149)

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    fyle_url: $ENV.FYLE_URL,
+    fyle_app_url: $ENV.FYLE_APP_URL,
+    fyle_client_id: $ENV.FYLE_CLIENT_ID,
+    callback_uri: $ENV.CALLBACK_URI,
+    api_url: $ENV.API_URL,
+    app_url: $ENV.APP_URL,
+    qbo_client_id: $ENV.QBO_CLIENT_ID,
+    qbo_scope: $ENV.QBO_SCOPE,
+    qbo_authorize_uri: $ENV.QBO_AUTHORIZE_URI,
+    qbo_app_url: $ENV.QBO_APP_URL,
+    hotjar_id: $ENV.HOTJAR_ID,
+    sentry_dsn: $ENV.SENTRY_DSN,
+    release: $ENV.RELEASE
+};

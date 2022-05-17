8e418dc683933b5dda77a588b933ea7549329ba6
ashwin1111 <winash1998@gmail.com>
2022-05-17 16:03:42 +0530

    replace env

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ do
     sed -i $SED_EXTRA_ARGS "s?{{API_URL}}?${API_URL}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{APP_URL}}?${APP_URL}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{FYLE_APP_URL}}?${FYLE_APP_URL}?g" $f;
+    sed -i $SED_EXTRA_ARGS "s?{{NEW_QBO_APP_URL}}?${NEW_QBO_APP_URL}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{CALLBACK_URI}}?${CALLBACK_URI}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{FYLE_CLIENT_ID}}?${FYLE_CLIENT_ID}?g" $f;
     sed -i $SED_EXTRA_ARGS "s?{{FYLE_URL}}?${FYLE_URL}?g" $f;
